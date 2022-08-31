937c56778a219531be721ae86685aaab6bfce68f
Tech Phoenix LTD <info@techphoenix.org>
2022-08-31 16:39:10 +0400

    commit

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,9 +1,35 @@
+import { XIcon } from '@heroicons/react/outline'
 import MuiModal from '@mui/material/Modal'
+import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
-import { modalState } from '../atoms/modalAtom'
+import { modalState, movieState } from '../atoms/modalAtom'
+import { Movie } from '../typing'
 
 const Modal = () => {
   const [showModal, setShowModal] = useRecoilState(modalState)
+  const [movie, setMovie] = useRecoilState(movieState)
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    if (!movie) return
+
+    const fetchMovie = async () => {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/${
+          movie?.media_type === 'tv' ? 'tv' : 'movie'
+        }/${movie?.id}?api_key=${
+          process.env.NEXT_PUBLIC_API_KEY
+        }&language=en-US&append_to_response=videos`
+      )
+        .then((response) => response.json())
+        .catch((err) => console.log(err.message))
+
+      setData(data)
+    }
+    fetchMovie()
+  }, [movie])
+
+  console.log(data)
 
   const handleClose = () => {
     setShowModal(false)
@@ -11,7 +37,15 @@ const Modal = () => {
 
   return (
     <MuiModal open={showModal} onClose={handleClose}>
-      <>Modal</>
+      <>
+        <button
+          onClick={handleClose}
+          className='modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818]'
+        >
+          <XIcon className='h-6 w-6' />
+        </button>
+        <div></div>
+      </>
     </MuiModal>
   )
 }
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { Movie } from '../typing'
 import { baseImageUrl } from '../constants/movie'
+import { useRecoilState } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom'
 
 interface Props {
   // movie: Movie | DocumentData
@@ -8,8 +10,17 @@ interface Props {
 }
 
 const Thumbnail = ({ movie }: Props) => {
+  const [showModal, setShowModal] = useRecoilState(modalState)
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
+
   return (
-    <div className='relative h-28 min-w-[180px] cursor-pointer transition duration-200 eas-out md:h-36  md:hover:scale-105'>
+    <div
+      className='relative h-28 min-w-[180px] cursor-pointer transition duration-200 eas-out md:h-36  md:hover:scale-105'
+      onClick={() => {
+        setCurrentMovie(movie)
+        setShowModal(true)
+      }}
+    >
       <Image
         src={`${baseImageUrl}${movie?.backdrop_path || movie?.poster_path}`}
         className='rounded-sm object-cover md:rounded'
